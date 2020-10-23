e9c5109355de6bd90d5bf8de58ce56dec85d7eb9
Hamza Yousuf <hamza.yousuf@treasurycube.com>
2020-10-23 11:17:04 +0500

    Complete Full auth system

--- script.js
+++ script.js
@@ -5,27 +5,41 @@ var msg = document.getElementById('msg')
 var loginmsg = document.getElementById('loginmsg')
 var login = document.getElementById('login')
 
-let data = localStorage.getItem('userrecord');
-
 login.addEventListener('submit', loginform = (e) => {
     e.preventDefault();
     var loginemail = document.getElementById('loginemail')
     var loginpassword = document.getElementById('loginpassword')
-        let data = localStorage.getItem('userrecord');
-        data = JSON.parse(data)
-        if(loginemail.value === data.email && loginpassword.value === data.password)
-        loginmsg.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+    let usersRecord = localStorage.getItem('userrecord');
+    usersRecord = JSON.parse(usersRecord)
+    for (let i = 0; i < usersRecord.length; i++) {
+        if (loginemail.value === usersRecord[i].email && loginpassword.value === usersRecord[i].password) {
+            loginmsg.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>Success</strong> Login Succesfully
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
         </div>`;
+        var currentUser = usersRecord[i].username;
+        sessionStorage.setItem('currentuser', currentUser)
         setTimeout(() => {
             location.href = 'home.html'
-        }, 2000);
+            }, 2000);
+        break;
+        
+
+        }
+        else {
+            loginmsg.innerHTML = `<br><div class="alert alert-danger alert-dismissible fade show" role="alert"><strong>Failed! </strong>Wrong Email and Password.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>`;
+
+            setTimeout(() => {
+                loginmsg.innerHTML = ''
+            }, 2000);
+
+        }
+    }
+
 });
-
-
-
-
-
--- signup.js
+++ signup.js
@@ -2,20 +2,47 @@ var signup = document.getElementById('signup')
 var username = document.getElementById('username')
 var email = document.getElementById('email')
 var password = document.getElementById('password')
+var users = [];
+let userFound = false;
+var emailError = document.getElementById('emailError')
+
+onEmailExist = () => {
+    var userEmailFound = false;
+
+    let usersRecord = localStorage.getItem('userrecord');
+    if (usersRecord) {
+        usersRecord = JSON.parse(usersRecord)
+        for (let i = 0; i < usersRecord.length; i++) {
+            if (email.value === usersRecord[i].email) {
+                emailError.innerHTML = '<span class="text-danger"><b>Email</b> Already Exist!!</span>';
+                email.focus();
+                userEmailFound = true;
+                break;
+            }
+        }
+
+        if (userEmailFound === false) {
+            emailError.innerHTML = '';
+        }
+    }
+}
 
 signup.addEventListener('submit', signupform = (e) => {
     e.preventDefault();
     if (username.value != '' && email.value != '' && password.value != '') {
         var userRecord = {
-            username : username.value,
-            email : email.value,
-            password : password.value
+            username: username.value,
+            email: email.value,
+            password: password.value
         }
-        userRecord = JSON.stringify(userRecord)
+        users.push(userRecord)
+        userRecord = JSON.stringify(users)
         localStorage.setItem('userrecord', userRecord)
-        
+        username.value = '' 
+        email.value = '' 
+        password.value = '';
 
-        msg.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        msg.innerHTML = `<br><div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>Success</strong> You Are Registered Success Fully.
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
